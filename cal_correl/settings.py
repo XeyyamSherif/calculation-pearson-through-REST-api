1992a4796724b778b0c41920b7cfe9e3476de742
XeyyamSherif <61112169+XeyyamSherif@users.noreply.github.com>
2022-01-17 22:03:46 +0400

    Update settings.py

--- cal_correl/settings.py
+++ cal_correl/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pznbef@#p80klcc!@np32%j!^j%asrg+=x!yu731tdy_kwi)#@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
